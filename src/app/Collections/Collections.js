93b879f85416325545699905abf40b1a74baf74f
Maricel Mendiola <icelmendiola22@yahoo.com>
2020-02-12 13:18:16 -0800

    Added more details

--- src/app/Collections/Collections.js
+++ src/app/Collections/Collections.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class Collections extends React.Component {
+export default class Collections extends Component {
   render () {
     return (
       <View style={styles.container}>
