b7d2ec1d7672d50d9366ead4323b1089f3c3dea7
Maricel Mendiola <icelmendiola22@yahoo.com>
2020-02-12 02:58:20 -0800

    Create data file for Bestsellers section

---
+++ src/app/Home/BestsellersData.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    img:
+      require('assets/testImage1.png'),
+    name: 'Item Name',
+    price: '$49.99'
+  },
+  {
+    img:
+      require('assets/testImage1.png'),
+    name: 'Item Name',
+    price: '$49.99'
+  },
+  {
+    img:
+      require('assets/testImage1.png'),
+    name: 'Item Name',
+    price: '$49.99'
+  },
+  {
+    img:
+      require('assets/testImage1.png'),
+    name: 'Item Name',
+    price: '$49.99'
+  },
+  {
+    img:
+      require('assets/testImage1.png'),
+    name: 'Item Name',
+    price: '$49.99'
+  },
+  {
+    img:
+      require('assets/testImage1.png'),
+    name: 'Item Name',
+    price: '$49.99'
+  },
+];
