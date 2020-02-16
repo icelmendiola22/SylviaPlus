d56136077075d5dbbd48ef8abfe2d438f1043a23
Maricel Mendiola <icelmendiola22@yahoo.com>
2020-02-15 23:34:06 -0800

    Create data file for Collections categories

---
+++ src/app/Collections/CollectionsData.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name1: 'Exotic Mixes',
+    img1: require('assets/exoticMixes.png'),
+    name2: 'Nature Beauty',
+    img2: require('assets/natureBeauty.png')
+  },
+  {
+    name1: 'Global Treasure',
+    img1: require('assets/globalTreasure.png'),
+    name2: 'Heritage Touch',
+    img2: require('assets/heritageTouch.png')
+  },
+  {
+    name1: 'Now and Then',
+    img1: require('assets/nowAndThen.png'),
+    name2: 'Old Things, New Ways',
+    img2: require('assets/oldThingsNewWays.png')
+  },
+  {
+    name1: 'World Traveller',
+    img1: require('assets/worldTraveller.png'),
+    name2: 'Turquoise, Indigo, Aqua And All Blue Hue',
+    img2: require('assets/blueHue.png')
+  },
+  {
+    name1: 'Bohemia',
+    img1: require('assets/bohemia.png'),
+    name2: 'Contemfusion',
+    img2: require('assets/contemfusion.png')
+  }
+];
