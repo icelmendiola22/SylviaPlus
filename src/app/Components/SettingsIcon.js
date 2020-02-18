7d877fa66595138ec32f275f720bcafbfe4b6b91
Maricel Mendiola <icelmendiola22@yahoo.com>
2020-02-17 23:49:00 -0800

    Added new component for icon

---
+++ src/app/Components/SettingsIcon.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { StyleSheet, Dimensions} from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+
+export default class AccountIcon extends Component {
+  render () {
+    return (
+      // Settings icon
+      <Ionicons
+        name="md-settings"
+        size={35}
+        style={styles.icon}
+      />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    color: 'rgba(0, 0, 0, 0.54)',
+    right: 10
+  }
+})
