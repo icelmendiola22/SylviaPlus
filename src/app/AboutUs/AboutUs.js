07ec71718f864141fda265d0133d19f1d8ecb842
Maricel Mendiola <icelmendiola22@yahoo.com>
2020-02-25 23:24:09 -0800

    Started and finished AboutUs page

--- src/app/AboutUs/AboutUs.js
+++ src/app/AboutUs/AboutUs.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Dimensions, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Image, ImageBackground, Text, View, SafeAreaView, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+// Import components for header
 import DrawerButton from 'app/Components/DrawerButton.js';
 import HeaderLogo from 'app/Components/HeaderLogo.js';
 import SearchBarIcon from 'app/Components/SearchBarIcon.js';
 import ShoppingCartIcon from 'app/Components/ShoppingCartIcon.js';
 
-
+// Get width of iPhone/Android screen
 var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
 
 export default class AboutUs extends Component {
   // toggleDrawer is a static function that controls the drawer menu button
@@ -17,14 +20,13 @@ export default class AboutUs extends Component {
   toggleSearch = () => {
     this.props.navigation.navigate('Ninth');
   }
-  // toggleSearch is a static function that navigates to the shopping cart page
+  // toggleCart is a static function that navigates to the shopping cart page
   toggleCart = () => {
     this.props.navigation.navigate('Eleventh');
   }
   render () {
     return (
       <View style={{backgroundColor: 'white', flex: 1}}>
-        <Text>About Us</Text>
         <SafeAreaView>
           {/* Header */}
           <View style={styles.headerContainer}>
@@ -51,41 +53,120 @@ export default class AboutUs extends Component {
               </View>
             </View>
           </View>
-          </SafeAreaView>
+
+          <View style={styles.scrollContainer}>
+            <View style={styles.bodyContainer}>
+              <ImageBackground
+                source={require('assets/bgPattern.png')}
+                style={styles.backgroundImage}
+              >
+                <ScrollView>
+                  <View style={{marginHorizontal: 10}}>
+                    <View style={styles.titleContainer}>
+                      <Text style={styles.title}>About Us</Text>
+                      <Text style={styles.quote}>“Home is where our heart is”</Text>
+                    </View>
+                    <Text style={styles.bodyTxt}>
+                      We believe style, fashion and great design can make life much
+                      more beautiful and colorful! Thats why here at Sylvia+, you will find
+                      an assortment of breath-taking contemporary to antique treasures, every
+                      element tells a story and culture around the world!
+                    </Text>
+                    <Image
+                      source={require('assets/aboutUsImage1.png')}
+                      style={styles.image}
+                    />
+                    <Text style={styles.bodyTxt}>
+                      All of our products are inspired by original designs, hand craft in mix media,
+                      and blends in east west culture, each of them to reflect our lifestyle, and the
+                      art of living.
+                    </Text>
+                    <Image
+                      source={require('assets/aboutUsImage2.png')}
+                      style={styles.image}
+                    />
+                    <Text style={styles.bodyTxt}>
+                      We hope you enjoy our products and service. And we look forward to meeting you
+                      in the near future, to share your story of Happy Life, and Philosophy of Artful
+                      Living!
+                    </Text>
+                  </View>
+                </ScrollView>
+              </ImageBackground>
+            </View>
           </View>
-          );
-          }
+        </SafeAreaView>
+      </View>
+    );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: Constants.statusBarHeight
   },
-    // Not using this style so might need to delete later on
-    headerContainer: {
-      height: 50
-    },
-    headerFlexRow: {
-      flex: 1,
-      flexDirection: 'row'
-    },
-    menuIcon: {
-      width: width / 8,
-      height: 50,
-    },
-    logoHeader: {
-      width: width/8 * 5,
-      height: 50,
-      alignItems: 'center',
-      justifyContent: 'center'
-    },
-    searchCartIcon: {
-      width: width/8 * 2,
-      height: 50,
-      flexDirection: 'row',
-      justifyContent: 'flex-end'
-    },
+  headerContainer: {
+    height: 50,
+    backgroundColor: 'white',
+  },
+  headerFlexRow: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  menuIcon: {
+    width: width / 8,
+    height: 50,
+  },
+  logoHeader: {
+    width: width/8 * 5,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  searchCartIcon: {
+    width: width/8 * 2,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  scrollContainer: {
+    height: height,
+    paddingBottom: 75,
+    backgroundColor: '#F4F4F4',
+  },
+  bodyContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+  },
+  titleContainer: {
+    marginTop: 40,
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: '#636363',
+    textDecorationLine: 'underline'
+  },
+  quote: {
+    fontSize: 23,
+    color: '#636363',
+    marginTop: 5,
+    fontStyle: 'italic'
+  },
+  bodyTxt: {
+    color:'#636363',
+    fontSize: 18,
+    marginVertical: 25
+  },
+  image: {
+    alignSelf: 'center'
+  }
 });
